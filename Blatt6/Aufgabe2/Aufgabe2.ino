26bc92e8df63eba09fb0927952e5c8b9832cd737
Jan-Hendrik Sünderkamp <jhsuenderkamp@googlemail.com>
2018-06-10 14:47:07 +0200

    fixed loop for a2

--- Blatt6/Aufgabe2/Aufgabe2.ino
+++ Blatt6/Aufgabe2/Aufgabe2.ino
@@ -13,18 +13,25 @@ void setup() {
   // init first two Fibonacci numbers
   fibData[0] = 1;
   fibData[1] = 2;
-  
+
   asm volatile(
     "ldr r4, [%[fD], #0]\n\t"
     "add.w r5, r4, %[lFi]\n\t"
     "back: mov r6, r4\n\t"
+    "ldr     r5, [%[lFi], #0]\n\t"
+    "cmp     r5, %[lFi]\n\t"
+    "bhi     exit\n\t"
     "adds r4, #1\n\t"
     "ldrb r7, [r6, #1]\n\t"
     "ldrb r6, [r6, #0]\n\t"
     "cmp r4, r5\n\t"
     "add r7, r6\n\t"
     "strb r7, [r4, #1]\n\t"
-    "bne.n back\n\t"
+    "ldr     r5, [%[lFi], #0]\n\t"
+    "add     r5, r5, #1\n\t"
+    "str     r5, [%[lFi], #0]\n\t"
+    "b       back\n\t"
+    "exit:\n\t"
     :: [fD] "r" (fibData[2]), [lFi] "r" (lastFiboIndex -2)
     : "r4" , "r5" , "r6" , "r7" , "cc" , "memory"
   );
@@ -33,7 +40,7 @@ void setup() {
 // main loop
 void loop() {
   // print data
-  for (int i = 0; i < lastFiboIndex; i++) 
+  for (int i = 0; i < lastFiboIndex; i++)
   {
     Serial.println(fibData[i]);
   }
