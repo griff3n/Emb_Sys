e83dda6c06857f1e68e42465b2e78c33683171e6
Peter Dick <pdick4@fh-bielefeld.de>
2018-06-17 20:26:56 +0200

    Aufgabe 1 ohne Interrupts :(

--- Blatt7/Aufgabe1/Aufgabe1.ino
+++ Blatt7/Aufgabe1/Aufgabe1.ino
@@ -2,21 +2,6 @@ const uint8_t ButtonPinb = PC_4;
 const uint8_t ButtonPind = PC_5;
 const uint8_t PBuzzerPin = PC_6;
 int state = 0;
-//loop variable and delay for debounce method
-#define debounceLoops 50
-#define debounceDelay 2
-
-uint8_t debounce(uint8_t PIN) {
-  int i;
-  for(i = 0; digitalRead(PIN) == HIGH && i < debounceLoops; i++) {
-    delay(debounceDelay);
-  }
-  if (i == debounceLoops) {
-    return HIGH;
-  } else {
-    return LOW;
-  }
-}
 
 template <const uint8_t PORT_NB>
 class TButton {
@@ -27,16 +12,14 @@ class TButton {
     }
 
     void updateState() {
-      if (debounce(PORT_NB) == HIGH) {
-        m_btnState = digitalRead(PORT_NB);
-        if (m_btnState != lastState) {
-          if (m_btnState == HIGH) {
-            state=state+stateAddition;
-          } else {
-            state=state-stateAddition;
-          }
-          lastState = m_btnState;
+      m_btnState = digitalRead(PORT_NB);
+      if (m_btnState != lastState) {
+        if (m_btnState == HIGH) {
+          state=state+stateAddition;
+        } else {
+          state=state-stateAddition;
         }
+        lastState = m_btnState;
       }
     }
 
@@ -70,22 +53,13 @@ TButton<ButtonPinb> buttonB = TButton<ButtonPinb>(LOW,1);
 TButton<ButtonPind> buttonD = TButton<ButtonPind>(LOW,2);
 PBuzzer<PBuzzerPin> pBuzzer;
 
-void updateBtnB() {
-  buttonB.updateState();
-}
-
-void updateBtnD() {
-  buttonD.updateState();
-}
-
 const int baudRate = 9600;
 void setup() {
   Serial.begin(baudRate);
-  //set interrupts
-  attachInterrupt(ButtonPinb, updateBtnB, RISING);
-  attachInterrupt(ButtonPind, updateBtnD, RISING);
 }
 void loop() {
+  buttonB.updateState();
+  buttonD.updateState();
   Serial.println(state);
   if (state == 3) {
     pBuzzer.setPBuzzerState(HIGH);
