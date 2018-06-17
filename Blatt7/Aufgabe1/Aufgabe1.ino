f929e82708300c25d0eb7d341336b03be78f3f70
Peter Dick <pdick4@fh-bielefeld.de>
2018-06-17 19:49:03 +0200

    Interrupts für Aufgabe 1

--- Blatt7/Aufgabe1/Aufgabe1.ino
+++ Blatt7/Aufgabe1/Aufgabe1.ino
@@ -2,6 +2,21 @@ const uint8_t ButtonPinb = PC_4;
 const uint8_t ButtonPind = PC_5;
 const uint8_t PBuzzerPin = PC_6;
 int state = 0;
+//loop variable and delay for debounce method
+#define debounceLoops 50
+#define debounceDelay 2
+
+uint8_t debounce(uint8_t PIN) {
+  int i;
+  for(i = 0; digitalRead(PIN) == HIGH && i < debounceLoops; i++) {
+    delay(debounceDelay);
+  }
+  if (i == debounceLoops) {
+    return HIGH;
+  } else {
+    return LOW;
+  }
+}
 
 template <const uint8_t PORT_NB>
 class TButton {
@@ -12,14 +27,16 @@ class TButton {
     }
 
     void updateState() {
-      m_btnState = digitalRead(PORT_NB);
-      if (m_btnState != lastState) {
-        if (m_btnState == HIGH) {
-          state=state+stateAddition;
-        } else {
-          state=state-stateAddition;
+      if (debounce(PORT_NB) == HIGH) {
+        m_btnState = digitalRead(PORT_NB);
+        if (m_btnState != lastState) {
+          if (m_btnState == HIGH) {
+            state=state+stateAddition;
+          } else {
+            state=state-stateAddition;
+          }
+          lastState = m_btnState;
         }
-        lastState = m_btnState;
       }
     }
 
@@ -53,28 +70,22 @@ TButton<ButtonPinb> buttonB = TButton<ButtonPinb>(LOW,1);
 TButton<ButtonPind> buttonD = TButton<ButtonPind>(LOW,2);
 PBuzzer<PBuzzerPin> pBuzzer;
 
+void updateBtnB() {
+  buttonB.updateState();
+}
+
+void updateBtnD() {
+  buttonD.updateState();
+}
 
 const int baudRate = 9600;
 void setup() {
   Serial.begin(baudRate);
+  //set interrupts
+  attachInterrupt(ButtonPinb, updateBtnB, RISING);
+  attachInterrupt(ButtonPind, updateBtnD, RISING);
 }
 void loop() {
-  //  if (buttonB.state() == HIGH && buttonD.state() == HIGH) {
-  //    state = 3;
-  //    Serial.println(state);
-  //  } else if (buttonB.state() == HIGH) {
-  //    state = 1;
-  //    Serial.println(state);
-  //  } else if (buttonD.state() == HIGH) {
-  //    state = 2;
-  //    Serial.println(state);
-  //  } else {
-  //    state = 0;
-  //    Serial.println(state);
-  //  }
-  buttonB.updateState();
-  buttonD.updateState();
-  
   Serial.println(state);
   if (state == 3) {
     pBuzzer.setPBuzzerState(HIGH);
